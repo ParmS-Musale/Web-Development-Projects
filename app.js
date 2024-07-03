b949a0628834138d59e53fcebc565e49372466b2
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-07-03 17:58:27 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelector('.dark-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    const icon = document.querySelector('.dark-toggle i');
-    icon.classList.toggle('fa-moon');
-    icon.classList.toggle('fa-sun');
-});
