bbc22cd56bfd6dbd3e0be5746423c4e77886b36d
PARMESHWAR MUSALE <152637541+ParmS-Musale@users.noreply.github.com>
2024-07-03 16:58:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 
+document.querySelector('.dark-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('.dark-toggle i');
+    icon.classList.toggle('fa-moon');
+    icon.classList.toggle('fa-sun');
+});
